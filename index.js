93d55e31693edbf9f5f2c6e349236e6549a9aea4
sebcod <sebastien.codus@live.fr>
2023-04-30 12:07:44 +0200

    changer de pays btn

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-
 const menu = document.querySelector("menu");
 const burger = document.querySelector(".menuBurger");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
+const btnFooterHeader = document.querySelector(".btnFooterHeader");
+const footerChangeCountry = document.querySelector(".footerChangeCountry");
 
-burger.addEventListener('click', () =>{
-    menu.classList.toggle('showMenu');
-    main.classList.toggle('blur');
-    footer.classList.toggle('blur');
-    
-     if(burger.classList.contains('aciveMenuBurger')){
-         burger.classList.toggle('disableMenuBurger');
-     }else{
-         burger.classList.toggle('aciveMenuBurger');
-     }
-})
+burger.addEventListener("click", () => {
+  menu.classList.toggle("showMenu");
+  main.classList.toggle("blur");
+  footer.classList.toggle("blur");
+
+  if (burger.classList.contains("aciveMenuBurger")) {
+    burger.classList.toggle("disableMenuBurger");
+  } else {
+    burger.classList.toggle("aciveMenuBurger");
+  }
+});
+
+btnFooterHeader.addEventListener("click", () => {
+  btnFooterHeader.classList.toggle("btnFooterHeaderActive");
+  footerChangeCountry.classList.toggle("footerChangeCountryActive");
+  window.scrollTo(0, document.body.scrollHeight);
+});
